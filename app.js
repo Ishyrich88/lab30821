788b4b706baf2ce48bfcfd0919fd02ed6b58e33e
Ishyrich88 <167790636+Ishyrich88@users.noreply.github.com>
2024-05-09 15:42:12 -0400

    figured max plant capacity

---
+++ app.js
@@ -0,0 +1,10 @@
+const PI =3.1415;
+const radius = 5;
+const area = PI * radius * radius;
+const plantSpace = 0.8;
+let initialPlantcount = 20;
+const plantDouble = 2;
+let plantGrowthweeks = 3
+const plantMaxcapacity = area/plantSpace;
+
+console.log(plantMaxcapacity);
