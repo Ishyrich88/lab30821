77fd5d845b0a8bc886a0cd511aaac1e691c03682
Ishyrich88 <167790636+Ishyrich88@users.noreply.github.com>
2024-05-09 23:08:55 -0400

    program

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const radius = 5;
 const area = PI * radius * radius;
 const plantSpace = 0.8;
 let initialPlantcount = 20;
+const startingarea = initialPlantcount*plantSpace
 const plantMaxcapacity = area/plantSpace;
 const week1growth = initialPlantcount*2;
 const week2growth = week1growth*2;
 const week3growth = week2growth*2;
 
-console.log(week2growth);
+console.log(startingarea)
